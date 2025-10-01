a9614ff1e5985b4691b696739414ac17bc3bc476
It24100007 <it24100007@my.sliit.lk>
2025-10-02 00:00:51 +0530

    Lab Sheet 09

---
+++ Lab Sheet 09/IT24100007_Lab09.R
@@ -0,0 +1,21 @@
+setwd("C:\\Users\\rt.x\\Desktop\\IT24100007_Lab09")
+getwd()
+
+#Exercise
+# i. Generate a random sample of size 25 for the baking time.
+bake <- rnorm(25, mean = 45, sd = 2)
+
+#view sample
+bake
+
+#ii. Test whether the average baking time is less than 46 minutes at a 5% level of
+#significance.
+res <- t.test(bake, mu = 46, alternative = "less")
+
+#view results
+res
+
+# Extract specific values
+res$statistic  
+res$p.value     
+res$conf.int    
